8ce1818d66f5cc5ca051edcfc804dcdad7de1988
Yadu Krishna <yaduk946@gmail.com>
2025-12-19 10:36:24 +0530

    Added nvchad config files to new branch

--- init.lua
+++ init.lua
@@ -1,15 +1,22 @@
--- bootstrap lazy.nvim, LazyVim and your plugins
-require("config.lazy")
-vim.opt.clipboard = "unnamedplus"
+require "core"
 
-vim.keymap.set("i", "<C-BS>", "<C-W>", { noremap = true }) -- Ctrl+Backspace
-vim.keymap.set("i", "<C-Del>", "<C-O>dw", { noremap = true }) -- Ctrl+Delete
+local custom_init_path = vim.api.nvim_get_runtime_file("lua/custom/init.lua", false)[1]
 
-if vim.g.neovide then
-  vim.g.neovide_scale_factor = 0.85 -- Adjust zoom level (0.9 for zoom out)
-  vim.g.neovide_fullscreen = true
-  vim.g.neovide_padding_top = 10
-  vim.g.neovide_scroll_animation_length = 0.2
-  vim.g.neovide_cursor_smooth_blink = true
-  vim.g.neovide_cursor_vfx_mode = "torpedo"
+if custom_init_path then
+  dofile(custom_init_path)
 end
+
+require("core.utils").load_mappings()
+
+local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
+
+-- bootstrap lazy.nvim!
+if not vim.loop.fs_stat(lazypath) then
+  require("core.bootstrap").gen_chadrc_template()
+  require("core.bootstrap").lazy(lazypath)
+end
+
+dofile(vim.g.base46_cache .. "defaults")
+vim.opt.rtp:prepend(lazypath)
+vim.opt.relativenumber = true
+require "plugins"
