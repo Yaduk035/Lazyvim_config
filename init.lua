ae455a9f52783c48662e606d7c787e180e524278
Yadu Krishna <yaduk946@gmail.com>
2026-03-14 10:30:44 +0530

    Added zoom in/out shortcut in neovide config.

--- init.lua
+++ init.lua
@@ -6,10 +6,27 @@ vim.keymap.set("i", "<C-BS>", "<C-W>", { noremap = true }) -- Ctrl+Backspace
 vim.keymap.set("i", "<C-Del>", "<C-O>dw", { noremap = true }) -- Ctrl+Delete
 
 if vim.g.neovide then
-  vim.g.neovide_scale_factor = 0.85 -- Adjust zoom level (0.9 for zoom out)
+  local scale_factor = 0.85
+  vim.g.neovide_scale_factor = scale_factor -- Adjust zoom level (0.9 for zoom out)
   vim.g.neovide_fullscreen = true
   vim.g.neovide_padding_top = 10
   vim.g.neovide_scroll_animation_length = 0.2
   vim.g.neovide_cursor_smooth_blink = true
   vim.g.neovide_cursor_vfx_mode = "torpedo"
+  vim.cmd.colorscheme("catppuccin-macchiato")
+
+  -- Zoom In
+  vim.keymap.set("n", "<C-=>", function()
+    vim.g.neovide_scale_factor = vim.g.neovide_scale_factor * 1.1
+  end)
+
+  -- Zoom Out
+  vim.keymap.set("n", "<C-->", function()
+    vim.g.neovide_scale_factor = vim.g.neovide_scale_factor / 1.1
+  end)
+
+  -- Reset Zoom
+  vim.keymap.set("n", "<C-0>", function()
+    vim.g.neovide_scale_factor = scale_factor
+  end)
 end
