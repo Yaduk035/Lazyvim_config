22a6a5927eec89951edf0281cc26ada358b17543
Yadu Krishna <yaduk946@gmail.com>
2026-08-16 20:50:31 +0530

    Added outline.nvim and undotree.nvim.

---
+++ lua/plugins/outline.lua
@@ -0,0 +1,10 @@
+return {
+  {
+    "hedyhli/outline.nvim",
+    cmd = { "Outline", "OutlineOpen" },
+    keys = {
+      { "<leader>o", "<cmd>Outline<cr>", desc = "Toggle Code Outline" },
+    },
+    opts = {},
+  },
+}
