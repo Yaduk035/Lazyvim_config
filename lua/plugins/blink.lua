54b21115fff16586ec7a61c68b1ebf2b6ea0a380
Yadu Krishna <yaduk946@gmail.com>
2026-08-18 17:56:35 +0530

    Fixed blink.cmp enter press issue.

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -4,6 +4,7 @@ return {
     opts = {
       keymap = {
         preset = "default",
+        ["<CR>"] = { "select_and_accept", "fallback" },
         ["<Tab>"] = { "select_next", "snippet_forward", "fallback" },
         ["<S-Tab>"] = { "select_prev", "snippet_backward", "fallback" },
       },
